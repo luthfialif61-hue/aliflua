8f21a5e68ab4164224ccb8ddac3f3ab4f546b4cc
luthfialif61-hue <luthfialif61@gmail.com>
2026-07-03 00:08:34 +0800

    Update alif lua.lua

--- alif lua.lua
+++ alif lua.lua
@@ -1,3 +1,8 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+getgenv().script_key = "HOSHI-KEY-65867cf342319da9"
+_G.script_key = getgenv().script_key
+loadstring(game:HttpGet("https://hoshihub.site/kaitun.lua"))()
+
 {
   "PLANT_SEED": {
     "Mushroom": 50,
@@ -75,8 +80,3 @@
     "Raccoon"
   ]
 }
-
-if not game:IsLoaded() then game.Loaded:Wait() end
-getgenv().script_key = "HOSHI-KEY-65867cf342319da9"
-_G.script_key = getgenv().script_key
-loadstring(game:HttpGet("https://hoshihub.site/kaitun.lua"))()
