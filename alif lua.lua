b80f8002b28ad226d06e3d3bd631f19a1c19a850
luthfialif61-hue <luthfialif61@gmail.com>
2026-07-03 00:39:19 +0800

    Update alif lua.lua

--- alif lua.lua
+++ alif lua.lua
@@ -1,4 +1,106 @@
-if not game:IsLoaded() then game.Loaded:Wait() end
-getgenv().script_key = "HOSHI-KEY-65867cf342319da9"
-_G.script_key = getgenv().script_key
-loadstring(game:HttpGet("https://hoshihub.site/kaitun.lua"))()
+_G.GAGConfig = _G.GAGConfig or {
+    ["Harvest"] = {
+        ["Auto Harvest"]  = true,
+        ["Sell At"]       = 95,
+        ["Sell Every"]    = 40,
+        ["Only Harvest"]  = {},
+        ["Don't Harvest"] = {},
+        ["Wait For Mutation"] = { "Mushroom", "Bamboo" }
+    },
+    ["Planting"] = {
+        ["Auto Plant"]  = true,
+        ["Plant Plan"]  = {
+            ["Gold"] = 200,
+            ["Rainbow"] = 200,
+        },
+
+        ["Only Plant"]  = {},
+        ["Layout"]      = "compact",
+        ["Don't Plant"] = {
+            ["Dragon's Breath"] = true,
+            ["Hypno Bloom"] = true,
+            ["Moon Bloom"] = true,
+            ["Venom Spitter"] = true,
+            ["Carrot"] = true,
+        },
+
+        ["Don't Buy"]   = {},
+        ["Keep Seeds"]  = {},
+        ["Plant Limit"] = {},
+        ["Never Shovel"] = { 
+            "Mushroom",
+            "Bamboo", 
+        },
+        ["Shovel Up To"] = "Uncommon",
+    },
+    ["Money"] = {
+        ["Keep Cash"]           = 15000,
+        ["Auto Expand Plot"]    = true,
+        ["Max Expansions"]      = 5,
+        ["Expand If Over"]      = 1500000,
+        ["Auto Replace Plants"] = false,
+    },
+    ["Never Sell"] = {
+        ["By Mutation"] = {},
+        ["By Fruit"]    = {},
+        ["Exact"]       = {},
+    },
+    ["Pets"] = {
+        ["Buy"] = {
+             ["Deer"] = 6,
+             "Raccoon",
+    },
+        ["Equip"]          = { Deer = 6 },
+        ["Auto Buy Slots"] = true,
+        ["Max Pet Slots"]  = 6,
+    },
+    ["Gear"] = {
+        ["Auto Buy"]             = true,
+        ["Keep Cash"]            = 15000,
+        ["Sprinkler Coverage"]   = "spread",
+        ["Place Sprinklers"]     = { ["Best"] = 6 },
+        ["Best Sprinkler Up To"] = "Rare Sprinkler",
+        ["Keep Gear"]            = { ["Trowel"] = 1 },
+        ["Buy Gear"]             = { "Super Watering Can", "Super Sprinkler" },
+    },
+    ["Event Seeds"] = {
+        ["Auto Claim"] = true,
+    },
+    ["Mail"] = {
+        ["Auto Claim"] = true,
+        ["Send To"]    = "x",
+        ["Send"]       = {
+            "Carrot", "Moon Bloom", "Dragon's Breath", "Gold", "Rainbow", "Venom Spitter", "Mega", "Hypno Bloom",
+            "Raccoon",
+            "Super Watering Can", "Super Sprinkler"
+        },
+    },
+    ["Misc"] = {
+        ["Auto Return To Garden"] = true,
+        ["Show Stats"]            = true,
+        ["Smart Travel"]          = true,
+        ["Auto Daily Deal"]       = true,
+        ["Walk Speed"]            = 35,
+        ["Slide Speed"]           = 35,
+        ["Fast Travel"]           = true,
+        ["Teleport"]              = false,
+    },
+    ["Friends"] = {
+        ["Auto Accept"] = false,
+        ["Auto Send"]   = false,
+    },
+    ["Performance"] = {
+        ["FPS Cap"]              = 5,
+        ["Low Graphics"]         = true,
+        ["Remove Other Gardens"] = true,
+        ["Hide Crop Visuals"]    = true,
+    },
+    ["Debug"] = {
+        ["Log To File"] = true,
+        ["Console"]     = true,
+    },
+}
+
+script_key="C121D2A194E1CA3545F329204D129455";
+
+local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/gag-obfuscated.lua")end)wait(1)until s;loadstring(r)()
